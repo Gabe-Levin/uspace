423af78b8d604ae1b6af622a332383e8b536b890
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-05 10:15:53 +0200

    added login and signup button tests

--- client/src/components/CreateSpaceForm/__test__/CreateSpaceForm.test.tsx
+++ client/src/components/CreateSpaceForm/__test__/CreateSpaceForm.test.tsx
@@ -11,42 +11,72 @@ const mockedUseNavigate = jest.fn();
 const propsMock = {
   setOpened: jest.fn(),
   allSpaces: [
-    {id: 111, name: 'space 1 name', createdAt: mockDate, userSpaceRoles: [
-      {user:{username:'billy', email: 'billy@billy.com'}},{user: {username:'sammy', email: 'sammy@gsammy.com'}}
-    ]},
-    {id: 222, name: 'space 2 name', createdAt: mockDate, userSpaceRoles: [
-      {user: {username:'ms', email: 'ms@ms.com'}}, {user: {username:'mavi', email: 'mavi@gmavi.com'}}
-    ]}
+    {
+      id: 111,
+      name: 'space 1 name',
+      createdAt: mockDate,
+      userSpaceRoles: [
+        { user: { username: 'billy', email: 'billy@billy.com' } },
+        { user: { username: 'sammy', email: 'sammy@gsammy.com' } },
+      ],
+    },
+    {
+      id: 222,
+      name: 'space 2 name',
+      createdAt: mockDate,
+      userSpaceRoles: [
+        { user: { username: 'ms', email: 'ms@ms.com' } },
+        { user: { username: 'mavi', email: 'mavi@gmavi.com' } },
+      ],
+    },
   ],
-  setAllSpaces: jest.fn()
-}
+  setAllSpaces: jest.fn(),
+};
 jest.mock('react-router', () => ({
-  ...jest.requireActual('react-router') as any,
-  useNavigate: () => mockedUseNavigate
+  ...(jest.requireActual('react-router') as any),
+  useNavigate: () => mockedUseNavigate,
 }));
 
-describe ('Testing CreateSpaceFrom component', () => {
+describe('Testing CreateSpaceFrom component', () => {
   afterEach(cleanup);
   test('contains a field to enter space name', async () => {
-    render(<CreateSpaceForm setOpened={propsMock.setOpened} allSpaces={propsMock.allSpaces} setAllSpaces={propsMock.setAllSpaces} />)
+    render(
+      <CreateSpaceForm
+        setOpened={propsMock.setOpened}
+        allSpaces={propsMock.allSpaces}
+        setAllSpaces={propsMock.setAllSpaces}
+      />
+    );
     const spaceNameInput = screen.getByLabelText(/space name:/i);
-    await userEvent.type(spaceNameInput, 'new space', {delay: 1});
+    await userEvent.type(spaceNameInput, 'new space', { delay: 1 });
     // @ts-ignore
     expect(spaceNameInput.value).toBe('new space');
-  })
+  });
   test('contains a field to enter space description', async () => {
-    render(<CreateSpaceForm setOpened={propsMock.setOpened} allSpaces={propsMock.allSpaces} setAllSpaces={propsMock.setAllSpaces} />)
+    render(
+      <CreateSpaceForm
+        setOpened={propsMock.setOpened}
+        allSpaces={propsMock.allSpaces}
+        setAllSpaces={propsMock.setAllSpaces}
+      />
+    );
     const spaceDescriptionInput = screen.getByLabelText(/description:/i);
-    await userEvent.type(spaceDescriptionInput, 'new space description', {delay: 1});
+    await userEvent.type(spaceDescriptionInput, 'new space description', { delay: 1 });
     // @ts-ignore
     expect(spaceDescriptionInput.value).toBe('new space description');
-  })
+  });
   test('snapshot matches, component renders correctly', () => {
     const tree = renderer
-      .create(<CreateSpaceForm setOpened={propsMock.setOpened} allSpaces={propsMock.allSpaces} setAllSpaces={propsMock.setAllSpaces} />)
+      .create(
+        <CreateSpaceForm
+          setOpened={propsMock.setOpened}
+          allSpaces={propsMock.allSpaces}
+          setAllSpaces={propsMock.setAllSpaces}
+        />
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
-  })
+  });
   // Not finished
   // test('prevents default on click', async () => {
   //   render(<CreateSpaceForm setOpened={propsMock.setOpened} allSpaces={propsMock.allSpaces} setAllSpaces={propsMock.setAllSpaces} />);
@@ -56,4 +86,4 @@ describe ('Testing CreateSpaceFrom component', () => {
   //   fireEvent.click(button, buttonClickEvent);
   //   expect(buttonClickEvent.preventDefault).toBeCalled();
   // })
-})
+});
